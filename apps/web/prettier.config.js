5b1048265a2a395f7cbec7d8be02fcd4f5f5e7d5
BurakGozen <info@newcode.be>
2023-01-04 12:33:13 +0100

    fix: initialLoader

--- apps/web/prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  printWidth: 100,
-  semi: true,
-  tabWidth: 4,
-  useTabs: false,
-  doubleQuote: true,
-  trailingComma: "all",
-  plugins: [require("prettier-plugin-tailwindcss")],
-};
--- packages/ui/src/components/layout/Root.tsx
+++ packages/ui/src/components/layout/Root.tsx
@@ -2,36 +2,31 @@ import dynamic from "next/dynamic";
 import { useRecoilState } from "recoil";
 import { scrollState } from "utils";
 
-const InitialLoader = dynamic(() =>
-	import("..").then((mod) => mod.InitialLoader)
-);
-// const PageTransition = dynamic(() =>
-//     import("ui").then((mod) => mod.PageTransition),
-// );
+const InitialLoader = dynamic(() => import("..").then((mod) => mod.InitialLoader));
 
 export default function Root({ children }: any) {
-	const [scroll] = useRecoilState(scrollState);
+    const [scroll] = useRecoilState(scrollState);
 
-	return (
-		<>
-			{process.env.NODE_ENV !== "development" || (true && <InitialLoader />)}
-			{/* <PageTransition /> */}
-			{children}
+    return (
+        <>
+            {process.env.NODE_ENV !== "development" && <InitialLoader />}
 
-			{!scroll.enabled && (
-				<style
-					// @ts-ignore
-					jsx
-					global
-				>
-					{`
-						body,
-						html {
-							overflow: hidden !important;
-						}
-					`}
-				</style>
-			)}
-		</>
-	);
+            {children}
+
+            {!scroll.enabled && (
+                <style
+                    // @ts-ignore
+                    jsx
+                    global
+                >
+                    {`
+                        body,
+                        html {
+                            overflow: hidden !important;
+                        }
+                    `}
+                </style>
+            )}
+        </>
+    );
 }
