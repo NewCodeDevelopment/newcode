7446deffdc58f52ea04ce7cb7fc661928c864b62
BurakGozen <info@newcode.be>
2023-01-06 17:39:50 +0100

    fix: en translations

--- apps/web/next-i18next.config.js
+++ apps/web/next-i18next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // https://www.i18next.com/overview/configuration-options#logging
     debug: false,
     i18n: {
-        locales: ["nl"],
+        locales: ["nl", "en"],
         defaultLocale: "nl",
         localeDetection: true,
         localePath: path.resolve("./public/locales"),
