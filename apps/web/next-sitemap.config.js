4f984f7959949b7bf3c92bad39ed6432b4bf292c
BurakGozen <info@newcode.be>
2023-01-06 20:23:17 +0100

    fix: sitemap ignore files

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || "https://newcode.be",
-    generateRobotsTxt: true, // (optional)
-    // ...other options
+    generateRobotsTxt: true,
+    generateIndexSitemap: true,
+    sitemapSize: 7000,
+    exclude: ["/api/*", "/connect/error", "/connect/success"],
 };
--- apps/web/pages/api/sitemap.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    // const sitemap = await generateSitemap();
-    res.setHeader("Content-Type", "text/xml");
-    // res.write(sitemap);
-    res.end();
-}
